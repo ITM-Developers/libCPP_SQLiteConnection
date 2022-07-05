0dcce7043c69c6850319918a31129d08e07c4a7b
ITM Developers <106715392+ITM-Developers@users.noreply.github.com>
2022-07-05 02:35:56 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+/* Conexion SQLite
+  ____                      _             ____   ___  _     _ _
+ / ___|___  _ __   _____  _(_) ___  _ __ / ___| / _ \| |   (_) |_ ___
+| |   / _ \| '_ \ / _ \ \/ / |/ _ \| '_ \\___ \| | | | |   | | __/ _ \
+| |__| (_) | | | |  __/>  <| | (_) | | | |___) | |_| | |___| | ||  __/
+ \____\___/|_| |_|\___/_/\_\_|\___/|_| |_|____/ \__\_\_____|_|\__\___|
+
+*/
+/*
+ConexionSQLite sqlite;
+sqlite.setDatabaseFile("testFiles/ChromeHistory");
+sqlite.openDataBase();
+sqlite.executeQuery("SELECT url FROM moz_places LIMIT 0,10");
+*/
+    return 0;
+}
